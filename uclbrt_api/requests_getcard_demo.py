18a0892036125959584d4beda9063e690a8d64b1
linyouwei <yjlyw020150@163.com>
2017-10-18 20:21:04 +0800

    123

--- uclbrt_api/requests_getcard_demo.py
+++ uclbrt_api/requests_getcard_demo.py
@@ -1,19 +1,20 @@
 #incoding=utf-8
-import requests
-import rsa
+import time
+import hashlib
+import base64
 import urllib
+import datetime
+import requests
+import json
 accountSid = '1f739496ba56392b914fa5f3b9ad5fdd'
 authToken = '9f52c2ab20db30e30a6ea6260771d9'
-publicKkey = "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqxqOJg" \
-             "0kqL4/xoNf0iDbjz/oM7ujsXOd92vQDkwO/rCP9wwZY0AvrMhcc56X4LmIbsbc1EZQ5ryMrIDbyCgtp" \
-             "gJJTQG/u/FBiwG2Yvqgx+9keVGZhBA+Oph34HFPWz4OEB+Py4QkaJPXALkjjh2Zf7Lgpv5gO8gRyg/o9FwC" \
-             "OZyEGiUmVorwPvwT3oMeNPCHxzlpGzdqV1kfqNmbS4ZkCiXGNhxxN0LJDnhaJJUl4bcnUjpcIxUlgSMX2" \
-             "CcooffIk3E1ROP051Xf/zmUWE6DTcGetf6ni2s2irDCgeanylyjLTgM6xaOYWqtG0yUC5lyzO46yTmE1Q4" \
-             "7XMM2h1KJswIDAQAB-----END PUBLIC KEY-----";
-print publicKkey
-info ="communityNo=1316880361&id=1f739496ba56392b914fa5f3b9ad5fdd&token=9f52c2ab20db30e30a6ea6260771d9&mobile=13480251015&areaCode=86&time=1508253409"
-data = rsa.encrypt(info.encode(),publicKkey)
-print data
-url = 'http://cz.uclbrt.com/apiLogin/?data='+data
-r = requests.get(url)
+body = {'mobile':'13480251015','communityNo':'1316880361','buildNo':'001','floorNo':'001','roomNo':'001','startTime':'1710031000','endTime':'1710231000','areaCode':'86'}
+s =datetime.datetime.now() + datetime.timedelta(hours=-6)
+batch = s.strftime("%Y%m%d%H%M%S")
+sig =hashlib.md5(accountSid+authToken+batch).hexdigest().upper()
+authen = base64.b64encode(accountSid+':'+batch)
+header = {'Accept':'application/json','Content-Type':'application/x-www-form-urlencode;charset=uft-8','Authorization':authen}
+url ='https://api.uclbrt.com:8058/?c=Qrcode&a=getLink&sig='+sig
+r = requests.post(url,json=body,headers=header,verify=False)
+#r = requests.post(url,data=json.dumps(body),headers=header,verify=False)
 print r.text
