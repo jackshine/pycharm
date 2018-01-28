b4bd8f69c184fb52b1d2f0adf68a4b47479a709e
linyouwei <yjlyw020150@163.com>
2018-01-29 00:30:35 +0800

    分页

--- django/Blog/blog/urls.py
+++ django/Blog/blog/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
 
-
 urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'^myblog/',include('myblog.urls')),
