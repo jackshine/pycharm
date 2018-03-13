cfce75a5483de4743cfe28e588ae805ed8a0e467
linyouwei <yjlyw020150@163.com>
2018-03-13 19:56:55 +0800

    接口测试

---
+++ uclbrt/interface_test/test.py
@@ -0,0 +1,60 @@
+import requests
+import json
+import unittest
+
+
+class MyTest(unittest.TestCase):  # 封装测试环境的初始化和还原的类
+    def setUp(self):  # 放对数据可操作的代码，如对mysql、momgodb的初始化等,这里不对数据库进行操作！
+        print("start test")
+        pass
+
+    def tearDown(self):  # 与setUp()相对
+        print("end test")
+        pass
+
+
+class test_building_get(MyTest):  # 把这个接口封装一个类，下面的方法是具体的测试用例
+    '''''接口名称：获取资质'''  # 这个描述接口名称
+    def test_login_token(self):
+        host = "115.29.142.212:8020"
+        link = "/mobile/user/login"
+        self.url = "http://"+host+link  # 请求url
+        self.data = {  # 请求参数
+            'areaCode':'86',
+            'account': '13480251015',
+            'l': 'zh_cn',
+            'passwd': 'J4oXyCwYfNiOD5FlhqL3agZRTP6Futhcsi52Rf+7mYPfo6Lb90xfdEnC/6OBsSqQ5y9b67YzEFPNaaK+p+KgeuNwmkjMkT0Zv61nUbS2YgNUk89DUALv8s0BYS3dnHEwLuy3In2vtOXCYdurgyT0CZWvX+glsKcMochghDAduuaP6dabFWSroPC1ZPuKl6k8YWr+8OVZyTW2NHiUzY9+KFo7NmUAvUagutit6iUzfwONAoA5JNIkb5Lz6mn2TWEriERdJb7pa6TSSsDBik40rw5IB0B+hSZChmlJKBdAwPXwQq2qGFyQQsu80hWvWCkWnx5jGLABqCI4qiHZ0EthLQ==',
+        }  # self.用在方法属性中，表示是该方法的属性，不会影响其他方法的属性。
+        r = requests.post(url=self.url, data=self.data)
+        print(json.loads(r.text)['data']['token'])
+
+
+    # def test_building_get(self):
+    #     '''''测试用例1：哈哈'''  # 这个描述接口用例名称
+    #     host = "115.29.142.212:8020"
+    #     link = "/Mobile/Room/addBuilding"
+    #     self.url = "http://" + host + link  # 请求url
+    #     print(self.url)
+    #     self.headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    #     self.data = {  # 请求参数
+    #         'id': '2427',
+    #         # 'token': """UbmWF6sqXkz9K2p3P62zePgufJw1usI7Aq/pGOtsRh5SuR4u7KaECea/1BHt/afoOQkEdA8cpAfOJ3w049LrS5iDarvJHkoC8alIpT7Zi6w9gbXrPorrZKGOyibDQNz7XVixcFMZh8gb3H9NogUelN+fa5hGfBBKMksMhMqgcf3Fvhuqlyk1BZlmwgsVV2pTkq10p8C8aD4ducsHxeVdHu2jGeZ3rCoCuePzvuliQ3x1zTyOFdQmi/5lFjZRLBfWKef4bD1wedqMjsJhfwKpC6SmCy+oXwRlJMAnL9w1RID05EcqVKwzaxQ9T7LyzuH0sJq/if9EXwaEmMm1WZZ2Mw==""",
+    #        'token':'vT0o9omn0HjCwemLvPpxQ+CualcqcjioC6HcTZQY79tmi4kYuhQCRQ07i+vbmS0HwP4jXbpeTj6TKHsYt8SLc9R5X7GvQHm/V8ymbJPgRkoQ1lYwYw0r+SU1O371M+3kKPDMQULm+iAiaWbmRHU0ysxpwEdz+PbfXaPCbgVan7gx86Rsr/CeycReUP4m9WyS1Ztdxw+z7ELTAK6vtsaRqE9GGkWy20aeAmXL0e0loiYBFZHMlw6VxNxh7yIYBV1Y3eyDK0x8RUbj6WCvSdQ0f+oxdWDLbnjBqnuoJwRBRwYUNaRPgyWFTCw8XOJNFwpkTHKrgeEB/itep8ndefFjuQ== ',
+    #         'name': '2427',
+    #         'num': '2',
+    #         'contact': 'www',
+    #         'areaCode': '86',
+    #         'phone': '13480251015',
+    #         'communityId':'640',
+    #         'desc': '',
+    #     }  # self.用在方法属性中，表示是该方法的属性，不会影响其他方法的属性。
+    #     print(json.dumps(self.data))
+    #     r = requests.post(url=self.url, data=self.data, headers=self.headers)
+    #     # return r.json()
+    #     print(r.text)
+    #     print()
+        # self.assertIn("true", self.r.text)  # 断言判断接口返回是否符合要求，可以写多个断言！
+
+
+if __name__ == "__main__":
+    unittest.main()
